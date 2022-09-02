ff3e04f3d58a97e82ad3d428808c86fb6d412754
MarinaMerzliakova <marinakms@yandex.ru>
2022-09-02 12:26:40 +0300

    Example011 Different Arrays

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{    int length = collection.Length;
+    int index = 0;
+    while (index < length) 
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+int [] array = new int[10];
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0; // это тоже как индекс, но называем иначе,т.к. переменные должны называться по-разному
+    while (position<count) 
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index<count)
+    {
+        if (collection[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+return position;
+}
+
+FillArray(array);
+array[4] = 4;
+array [6] = 4;
+PrintArray(array);
+
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
